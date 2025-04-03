544ca44cfe35d54f4de439ea28175712d78a4eb9
Deepak Kaligotla <deepak.kaligotla@gmail.com>
2025-04-03 21:43:15 +0530

    secret

--- routes/server.js
+++ routes/server.js
@@ -1,9 +1,8 @@
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
+console.log(process.env.PORT)
 
-const port = process.env.PORT || 4000;
-
-server.listen(port, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Server running on port localhost`);
 });
